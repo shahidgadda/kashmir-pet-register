64140803a69eb53f564403d7b19d99363bdf51e4
Shahid Ayoub <shahidgadda1@gmail.com>
2026-02-02 20:01:31 +0530

    Update index.ts

--- db/index.ts
+++ db/index.ts
@@ -10,6 +10,9 @@ if (!process.env.DATABASE_URL) {
 
 const client = new pg.Client({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 client.connect();
