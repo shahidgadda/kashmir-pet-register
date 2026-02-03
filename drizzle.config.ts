7cab32142ab645159ba79303aa53ebb4874140fb
Shahid Ayoub <shahidgadda1@gmail.com>
2026-02-03 11:11:21 +0530

    Update drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -9,6 +9,9 @@ export default defineConfig({
   schema: "./shared/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.DATABASE_URL,
+  url: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
   },
+},
 });
